811b1b6e6c20cea5eeaedc6f2e417af619364804
jtovar2 <jtovar2@student.gsu.edu>
2016-01-17 20:12:24 -0500

    fixed PostApi.java

--- src/main/java/javier/loyaltynetwork/PostApi.java
+++ src/main/java/javier/loyaltynetwork/PostApi.java
@@ -1,7 +1,9 @@
 package javier.loyaltynetwork;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import javier.loyaltynetwork.databaseApi.DatabaseApi;
